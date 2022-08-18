e6399cfb8f0a3035da0854bf29a4f4a54a77f3d5
Krzysztof Rams <krzysztofeczek@gmail.com>
2022-08-18 14:08:07 +0200

    1. Multiples of 3 or 5 - refactor

--- p000001/main.py
+++ p000001/main.py
@@ -1,16 +1,24 @@
-def search_numbers_sum(range_limit: int):
+def is_multiplication_of_any_factor(factors: list[int], checked_number: int):
+    multiplications: list[int] = []
+    for current_factor in factors:
+        is_multiplication: bool = checked_number % current_factor == 0
+        if is_multiplication:
+            multiplications.append(current_factor)
+    return multiplications
+
+
+def search_numbers_sum(range_limit: int, factors: list[int]):
     searched_numbers: list[int] = []
     searched_sum: int = 0
     for n in range(1, range_limit):
-        multiply_of_three: bool = n % 3 == 0
-        multiply_of_five: bool = n % 5 == 0
-        if multiply_of_five or multiply_of_three:
+        multiplications: list[int] = is_multiplication_of_any_factor(factors, n)
+        if len(multiplications) > 0:
             searched_numbers.append(n)
             searched_sum += n
 
     return searched_sum
 
 
-print(f'searched_numbers sum : {search_numbers_sum(10)}')
+print(f'searched_numbers sum : {search_numbers_sum(10, [3, 5])}')
 
-print(f'searched_numbers sum : {search_numbers_sum(1000)}')
+print(f'searched_numbers sum : {search_numbers_sum(1000, [3, 5])}')
